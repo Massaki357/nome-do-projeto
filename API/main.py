7e600ed2c58adedbf320ed965a56bdbdeb9a52d4
Massaki <82161746+Massaki357@users.noreply.github.com>
2026-09-09 18:30:41 -0300

    2

--- API/main.py
+++ API/main.py
@@ -3,11 +3,19 @@ import os
 import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class MessageRequest(BaseModel):
